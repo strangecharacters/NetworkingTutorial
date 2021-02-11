410eca0cd6ce803fc37be6c09a6dc4034d09a1d1
tmd at StrangeCharacters <tmd+git@strangecharacters.com>
2021-02-11 16:31:23 +0000

    Working cube movement over network

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+   void HandleMovement()
+   {
+       if(isLocalPlayer)
+       {
+           float speed = 4;
+           float moveHorizontal = Input.GetAxis("Horizontal");
+           float moveVertical = Input.GetAxis("Vertical");
+
+           Vector3 movement = new Vector3(moveHorizontal * Time.deltaTime * speed,
+                                         moveVertical * Time.deltaTime * speed,
+                                         0);
+           transform.position = transform.position += movement;
+           
+       }
+   }
+
+   void Update() 
+   {
+       HandleMovement();
+   }
+}
