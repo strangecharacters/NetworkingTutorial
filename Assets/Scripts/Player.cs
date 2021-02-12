1e3fba21a93d583b5b885985117d1d7d4fe5da33
tmd at StrangeCharacters <tmd+git@strangecharacters.com>
2021-02-12 17:27:06 +0000

    Hola command working and replying via targetrpc

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,35 +5,55 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
-   void HandleMovement()
-   {
-       if(isLocalPlayer)
-       {
-           float speed = 4;
-           float moveHorizontal = Input.GetAxis("Horizontal");
-           float moveVertical = Input.GetAxis("Vertical");
+    void HandleMovement()
+    {
+        if(isLocalPlayer)
+        {
+            float speed = 4;
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-           Vector3 movement = new Vector3(moveHorizontal * Time.deltaTime * speed,
-                                         moveVertical * Time.deltaTime * speed,
-                                         0);
-           transform.position = transform.position += movement;
-           
-       }
-   }
+            Vector3 movement = new Vector3(moveHorizontal * Time.deltaTime * speed,
+                                        moveVertical * Time.deltaTime * speed,
+                                        0);
+            transform.position = transform.position += movement;
+/* Silly to call this in an update loop, spamming the network
+        if(isServer && transform.position.y > 50)
+        {
+        TooHigh(); // this is called on server but runs on all clients [ClientRPC]
+        }
+*/
+        }
+    }
 
-   void Update() 
-   {
-       HandleMovement();
-       if(isLocalPlayer && Input.GetKeyDown(KeyCode.X))
-       {
-           Debug.Log("Sending Hola to the server.");
-           Hola(); // call it from the client, nothing will happen on the client though
-       }
-   }
+    void Update() 
+    {
+        HandleMovement();
+        if(isLocalPlayer && Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Sending Hola to the server.");
+            Hola(); // call it from the client, nothing will happen on the client though
+        }
+    }
 
-   [Command]
+    [Command]
     void Hola()
     {
         Debug.Log("Received Hola from client"); // this will run on the server when it has been called from the client
+        ReplyHola(); // we call via TargetRPC and this will run on the calling client - sends to the original calling client by default
+            // but the function can take a conn so the client can be specified
     }
+
+    [ClientRpc]
+    void TooHigh()
+    {
+        Debug.Log("Too High");
+    }
+
+    [TargetRpc]
+    void ReplyHola()
+    {
+        Debug.Log("Received Hola from server");
+    }
+
 }
